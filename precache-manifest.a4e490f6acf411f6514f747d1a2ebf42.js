a7af238254643c0e2e6350dfcfdc60d6920e8082
shashikant <shashi94goswami@gmail.com>
2020-05-26 20:34:26 +0530

    Updates

---
+++ precache-manifest.a4e490f6acf411f6514f747d1a2ebf42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5fc522591ab1a5bf272abdbf327b819",
+    "url": "/monsterlist/index.html"
+  },
+  {
+    "revision": "adf1fe406067af672618",
+    "url": "/monsterlist/static/css/main.a012d3e6.chunk.css"
+  },
+  {
+    "revision": "023aa596863420641f2d",
+    "url": "/monsterlist/static/js/2.4d1e493d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterlist/static/js/2.4d1e493d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf1fe406067af672618",
+    "url": "/monsterlist/static/js/main.7e20cbd4.chunk.js"
+  },
+  {
+    "revision": "e66bd91312026529fecf",
+    "url": "/monsterlist/static/js/runtime-main.b6774412.js"
+  }
+]);
