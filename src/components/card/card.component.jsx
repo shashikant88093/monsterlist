e8e07a201f87b7eccb1f430246cbe129342486e0
shashikant <shashi94goswami@gmail.com>
2020-05-26 21:04:56 +0530

    hellO

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ export const Card = (props) => (
   <div className="card-container">
     <img
       alt="monster"
-      src={`https:robohash.org/${props.monster.id}?set=set2`}
+      src={`https://source.unsplash.com/random/${props.monster.id}?set=set2`}
     />
     <h2>{props.monster.name}</h2>
     <p>{props.monster.email}</p>
